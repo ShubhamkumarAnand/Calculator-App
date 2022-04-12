d0b7e7764e218b988071d06f34a9f550864a36c5
imskanand <the.anand3099@gmail.com>
2022-04-13 00:25:51 +0530

    Calculator App ++ UI Done ++ Button Add Funcitionality Done ++ All the Funcationality Done //TODO : Equal Funcationality to be Implemented

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,55 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onDigit(view: View){
+        val tvInput = findViewById<TextView>(R.id.tvInput)
+        tvInput.append((view as Button).text)
+        lastNumeric = true
+    }
+    fun onClear(view: View){
+        val tvInput = findViewById<TextView>(R.id.tvInput)
+        tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
+
+    fun onDecimalPoint(view: View){
+        val tvInput = findViewById<TextView>(R.id.tvInput)
+        if(lastNumeric && !lastDot){
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot= true
+        }
+    }
+
+    fun onOperator(view: View){
+        val tvInput = findViewById<TextView>(R.id.tvInput)
+        if(lastNumeric && !isOperatorAdded(tvInput.text.toString())){
+            tvInput.append((view as Button).text)
+            lastDot = false
+            lastNumeric = false
+        }
+    }
+
+    private fun isOperatorAdded(value:String):Boolean{
+        return if(value.startsWith("-")){
+            false
+        }else{
+            value.contains("/") || value.contains("*")
+                    || value.contains("-") || value.contains("+")
+        }
     }
 }
